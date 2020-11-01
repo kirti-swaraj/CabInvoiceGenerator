2eb26766a76dd54c7a055483abbf8fa26603f6c3
Kirti Swaraj <Sai@KirtiSwaraj>
2020-11-01 20:22:53 +0530

    [kirti] Add . code to generate invoice for premium and normal ride

---
+++ CabInvoiceGenerator/RideTypecs.cs
@@ -0,0 +1,21 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="RideType.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Kirti Swaraj"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CabInvoiceGenerator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    /// <summary>
+    /// UC 5 : RideType defined as NORMAL and PREMIUM
+    /// </summary>
+    public enum RideType
+    {
+        NORMAL,
+        PREMIUM,
+        WRONG_RIDE_TYPE
+    }
+}
